9c4239f1e673309f205fc78dbc4646dee7dad0c0
fengwuJ <1533149618@qq.com>
2019-04-15 18:12:49 +0800

    风舞J第一次修改

--- src/main/java/com/xzt/Application.java
+++ src/main/java/com/xzt/Application.java
@@ -3,7 +3,9 @@ package com.xzt;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
 @SpringBootApplication
 @MapperScan("com.xzt.mapper")
 public class Application {
